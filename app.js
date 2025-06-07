a52179bd984c25a049432d13da88cf95a86a4bc5
smerdyakov-moi <pragyankhati45@gmail.com>
2025-06-07 16:17:34 +0545

    SQL Queries

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require ('express')
 const db = require ('./config/mongoose-connection')
 const cookieParser = require('cookie-parser')
 const adminModel = require ('./models/adminModel')
-
+const cors = require('cors')
 const app = express()
 
+app.use(cors({origin: 'http://127.0.0.1:5500',credentials: true}))
 app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -19,4 +20,4 @@ app.use('/',userRoutes)
 
 
 
-app.listen('3000',()=>{console.log("Listening on port 3000!!")})
+app.listen('5000',()=>{console.log("Listening on port 5000!!")})
