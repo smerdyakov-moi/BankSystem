1f9bddeeb74f48bd3f6662852ade14465e5a2170
smerdyakov-moi <pragyankhati45@gmail.com>
2025-07-14 10:37:53 +0545

    Hopefully final token auth

--- bank-backend/controllers/authController.js
+++ bank-backend/controllers/authController.js
@@ -1,26 +1,23 @@
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const userModel = require('../models/userModel');
 const adminModel = require('../models/adminModel');
-const notallowedModel = require ('../models/notallowedModel')
-
-const {generateUserToken,generateAdminToken} = require('../utils/tokenregistration')
+const notallowedModel = require ('../models/notallowedModel');
+const { generateUserToken, generateAdminToken } = require('../utils/tokenregistration');
 
 const registerUser = async (req, res) => {
-  
-  const { name, password,email} = req.body;
+  const { name, password, email } = req.body;
+  const accountNumber = Math.floor(100000000 + Math.random() * 900000000);
+  let balance = 0;
 
-  const accountNumber = Math.floor(100000000 + Math.random() * 900000000)
-  let balance=0
   if (await userModel.findOne({ email })) {
     return res.status(400).send('User already exists');
   }
-  if(await userModel.findOne({accountNumber})){
-    return res.status(500).send('Please try again!!')
+  if (await userModel.findOne({ accountNumber })) {
+    return res.status(500).send('Please try again!!');
   }
   try {
     const hash_pw = await bcrypt.hash(password, 10);
-    await userModel.create({ name, email, password: hash_pw ,accountNumber,balance});
+    await userModel.create({ name, email, password: hash_pw, accountNumber, balance });
     res.send('User successfully created');
   } catch (err) {
     console.error(err);
@@ -34,47 +31,44 @@ const loginUser = async (req, res) => {
   if (!user) return res.status(400).send('No user with such email');
 
   const result = await bcrypt.compare(password, user.password);
-  if (result) {
-    const token = generateUserToken({ email, userid: user._id });
-    const isSecure = req.headers.host && req.headers.host.includes('https');
+  if (!result) return res.status(400).send('Incorrect credentials');
 
-    res.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 3600000,
-      secure: isSecure,
-      sameSite: isSecure ? 'none' : 'lax',
-    });
+  const token = generateUserToken({ email, userid: user._id });
 
-    res.json('User logged in successfully');
-  } else {
-    res.status(400).send('Incorrect credentials');
-  }
+  res.cookie('token', token, {
+    httpOnly: true,
+    maxAge: 3600000,
+    secure: true,
+    sameSite: 'none',
+    path: '/'
+  });
+
+  res.json('User logged in successfully');
 };
 
 const logoutUser = (req, res) => {
-  const isSecure = req.headers.host && req.headers.host.includes('https');
-
   res.clearCookie('token', {
     httpOnly: true,
-    secure: isSecure,
-    sameSite: isSecure ? 'none' : 'lax',
-  });
-
-  res.json('User logged out successfully');
+    secure: true,
+    sameSite: 'none',
+    path: '/'
+  }).json('User logged out successfully');
 };
 
-
 const createAdmin = async (req, res) => {
-  const {email, password } = req.body;
-  if(await notallowedModel.findOne({email})){
-    let notadmin = notallowedModel.findOne({email})
-    return res.send("WARNING!!! YOU ARE NOT ALLOWED!!!")}
+  const { email, password } = req.body;
 
-  if(await adminModel.findOne({email})){return res.send("ERROR")}
-  permission = false
+  if (await notallowedModel.findOne({ email })) {
+    return res.send('WARNING!!! YOU ARE NOT ALLOWED!!!');
+  }
+
+  if (await adminModel.findOne({ email })) {
+    return res.send('ERROR');
+  }
+
   try {
     const hash_pw = await bcrypt.hash(password, 10);
-    await adminModel.create({email, password: hash_pw,permission });
+    await adminModel.create({ email, password: hash_pw, permission: false });
     res.send('Account created. Check to see whether you are allowed or not. (If you are not allowed once, do not attempt again! Necessary actions will be taken otherwise.');
   } catch (err) {
     console.error(err);
@@ -83,44 +77,58 @@ const createAdmin = async (req, res) => {
 };
 
 const loginAdmin = async (req, res) => {
+  const { email, password } = req.body;
 
-  const { email, password } = req.body
-  if(await notallowedModel.findOne({email})){return res.send("WARNING!!! YOU ARE NOT ALLOWED!!!")}
+  if (await notallowedModel.findOne({ email })) {
+    return res.send('WARNING!!! YOU ARE NOT ALLOWED!!!');
+  }
 
-  const admin = await adminModel.findOne({email})
+  const admin = await adminModel.findOne({ email });
 
-  if (!admin) return res.status(400).json('You haven\'t attempted to access admin priviliges.') 
-  if(admin.permission!==true){return res.send("You are currently not allowed admin priviliges")}
+  if (!admin) return res.status(400).json("You haven't attempted to access admin privileges.");
+  if (!admin.permission) return res.send('You are currently not allowed admin privileges');
 
   const result = await bcrypt.compare(password, admin.password);
-  if (result) {
-    const token = generateAdminToken({ email, adminid: admin._id })
-    res.cookie('token', token, { httpOnly: true, maxAge: 3600000, secure: false, sameSite: 'strict' })
-      .json('Admin logged in successfully');
-  } else {
-    res.status(400).json('Incorrect credentials');
-  }
+  if (!result) return res.status(400).json('Incorrect credentials');
+
+  const token = generateAdminToken({ email, adminid: admin._id });
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    maxAge: 3600000,
+    secure: true,
+    sameSite: 'none',
+    path: '/'
+  });
+
+  res.json('Admin logged in successfully');
 };
 
 const logoutAdmin = (req, res) => {
-  res.clearCookie('token', { httpOnly: true, secure: false }).json('Admin logged out successfully');
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    path: '/'
+  }).json('Admin logged out successfully');
 };
 
-const permissionAdmin = async (req,res)=>{
-    if (req.admin.email !=='admin-1@gmail.com'){return res.send("You are not allowed to give permissions")}
-    const {email,permission} = req.body
-    console.log(permission)
-    decision = await adminModel.findOne({email})
-    if(permission){
-        decision.permission=true
-        await decision.save()
-        return res.send(`${email} allowed admin priviliges`)
-    }else{
-        let notallowed = await notallowedModel.create({email})
-        let deleted = await adminModel.findOneAndDelete({email})
-        return res.send(`${email} not allowed admin priviliges`)
-    }
-}
+const permissionAdmin = async (req, res) => {
+  if (req.admin.email !== 'admin-1@gmail.com') return res.send('You are not allowed to give permissions');
+
+  const { email, permission } = req.body;
+  const decision = await adminModel.findOne({ email });
+
+  if (permission) {
+    decision.permission = true;
+    await decision.save();
+    return res.send(`${email} allowed admin privileges`);
+  } else {
+    await notallowedModel.create({ email });
+    await adminModel.findOneAndDelete({ email });
+    return res.send(`${email} not allowed admin privileges`);
+  }
+};
 
 module.exports = {
   registerUser,
