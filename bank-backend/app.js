e753ef22d29af53b0464dbd75d7a70fa0bb56b1c
smerdyakov-moi <pragyankhati45@gmail.com>
2025-07-14 08:49:01 +0545

    FrontEnd updated domain

--- bank-backend/app.js
+++ bank-backend/app.js
@@ -7,7 +7,7 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.use(cors({ origin: 'http://localhost:3001', credentials: true }))
+app.use(cors({ origin: 'https://banksystem-frontend.onrender.com', credentials: true }))
 app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
