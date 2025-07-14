d3498decc674af3297b71faebd5369be96bc9599
smerdyakov-moi <pragyankhati45@gmail.com>
2025-07-14 09:33:24 +0545

    MongooseConnectionUpdate

--- bank-backend/config/mongoose-connection.js
+++ bank-backend/config/mongoose-connection.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
-
 const dbgr = require("debug")("development:mongoose");
 
 const mongoURI = process.env.MONGODB_URI;
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => dbgr("MongoDB connected"))
-.catch((err) => console.error("MongoDB connection error:", err));
+mongoose
+  .connect(mongoURI)
+  .then(() => dbgr("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 module.exports = mongoose.connection;
