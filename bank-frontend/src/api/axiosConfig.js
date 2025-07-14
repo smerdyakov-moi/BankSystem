bd36e038cc0b2cd5b6adf15ec7f93aba023c5224
smerdyakov-moi <pragyankhati45@gmail.com>
2025-07-14 08:42:25 +0545

    Updated LiveService BackEnd

--- bank-frontend/src/api/axiosConfig.js
+++ bank-frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://banksystem-k2r2.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
