6f3ecce9c0be268bbe3b6ca8cb3bce18ea1bae53
smerdyakov-moi <pragyankhati45@gmail.com>
2025-07-14 10:16:38 +0545

    MongooseConnection reconfigured for checking render logs

--- bank-backend/config/mongoose-connection.js
+++ bank-backend/config/mongoose-connection.js
@@ -1,14 +1,17 @@
-require('dotenv').config()
+require("dotenv").config();
+console.log("Connecting to:", process.env.MONGODB_URI);
 
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 
 const mongoURI = process.env.MONGODB_URI;
 
-
 mongoose
   .connect(mongoURI)
-  .then(() => dbgr("MongoDB connected"))
+  .then(() => {
+    dbgr("MongoDB connected");
+    console.log("✅ DB Name:", mongoose.connection.name);
+  })
   .catch((err) => console.error("MongoDB connection error:", err));
 
 module.exports = mongoose.connection;
