7a232404ddac382540ce3592173a01507548c2df
smerdyakov-moi <pragyankhati45@gmail.com>
2025-07-14 09:44:15 +0545

    Mongoose-3

--- bank-backend/config/mongoose-connection.js
+++ bank-backend/config/mongoose-connection.js
@@ -1,11 +1,21 @@
-const mongoose = require("mongoose");
-const dbgr = require("debug")("development:mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const mongoURI = process.env.MONGODB_URI;
 
-mongoose
-  .connect(mongoURI)
-  .then(() => dbgr("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+const connectWithTimeout = async () => {
+  console.log('Starting mongoose connect...');
+  try {
+    await Promise.race([
+      mongoose.connect(mongoURI),
+      new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout connecting to MongoDB')), 10000))
+    ]);
+    console.log('MongoDB connected successfully!');
+    process.exit(0); // exit after success
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // exit with failure
+  }
+};
 
-module.exports = mongoose.connection;
+connectWithTimeout();
